0052826d911653647db9ccb30c610aaf4cfcc148
Bharathi.Valluri <you@example.com>
2022-12-01 19:21:59 +0530

    modified

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const multer = require('multer')
-const tiff_to_png = require('tiff-to-png')
+const Jimp = require('jimp')
+// const tiff_to_png = require('tiff-to-png')
 const path = require('path')
 const storage = multer.diskStorage({
   destination: './uploads',
@@ -19,7 +20,7 @@ var upload = multer({
     fieldNameSize: 50, // TODO: Check if this size is enough
     fieldSize: 20000, //TODO: Check if this size is enough
     // TODO: Change this line after compression
-    fileSize: 1200000
+    fileSize: 1200000 // 120 KB for a 1080x1080 JPG 90
   },
   fileFilter: function (_req, file, cb) {
     checkFileType(file, cb)
@@ -48,22 +49,30 @@ router.post(
 )
 
 // converting tiff file to png format
-const options = {
-  logLevel: 1
-}
+// const options = {
+//   logLevel: 1
+// }
 
-const converter = new tiff_to_png(options)
-const tiffsLocation = ['source/uploads/file-1669885431273.tiff']
-const location = './uploads'
-converter.convertArray(tiffsLocation, location)
-router.post(
-  '/convertToPng',
-  converter.convertArray(tiffsLocation, location),
-  (req, res) => {
-    res.status(200).json({
-      message: 'tiff converted into PNG successfully'
-    })
-  }
-)
+// const converter = new tiff_to_png(options)
+// const tiffsLocation = ['source/uploads/file-1669885431273.tiff']
+// const location = './uploads'
 
-module.exports = router
+// converter.convertArray(tiffsLocation, location)
+
+//
+//     })
+//   }
+// )
+
+// Jimp.read('profile-1669894444550.tiff', function (err, file) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     file.write('new-image.png')
+//   }
+//   res.status(200).json({
+//     message: 'tiff converted into PNG successfully'
+//   })
+// })
+// router.post('/convertToPNG', Jimp.read('output'))
+// module.exports = router
